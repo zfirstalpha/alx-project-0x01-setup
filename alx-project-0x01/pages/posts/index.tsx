d90f6ef2b9c9fc756225d0b29889397023acb4f9
zfirstalpha <salim14sadik@gmail.com>
2025-10-02 16:12:51 +0300

    navbar added

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,5 +1,14 @@
+import Header from "@/components/layout/Header";
+
 const PostsPage: React.FC = () => {
-  return <h1>Posts Page</h1>;
+  return (
+    <div className="flex flex-col h-screen">
+      <Header />
+      <main className="flex-grow flex items-center justify-center">
+        <h1 className="text-3xl font-bold">Posts Page</h1>
+      </main>
+    </div>
+  );
 };
 
 export default PostsPage;
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,5 +1,14 @@
+import Header from "@/components/layout/Header";
+
 const UsersPage: React.FC = () => {
-  return <h1>Users Page</h1>;
+  return (
+    <div className="flex flex-col h-screen">
+      <Header />
+      <main className="flex-grow flex items-center justify-center">
+        <h1 className="text-3xl font-bold">Users Page</h1>
+      </main>
+    </div>
+  );
 };
 
 export default UsersPage;
