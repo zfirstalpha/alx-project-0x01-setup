50ea72150320d784b71d727125e685596d4e13d0
zfirstalpha <salim14sadik@gmail.com>
2025-10-02 16:20:08 +0300

    feat(posts): implement PostCard component and display posts list

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,14 +1,46 @@
+import PostCard from "@/components/common/PostCard";
 import Header from "@/components/layout/Header";
+import { PostProps } from "@/interfaces";
 
-const PostsPage: React.FC = () => {
+interface PostsPageProps {
+  posts: PostProps[];
+}
+
+const Posts: React.FC<PostsPageProps> = ({ posts }) => {
+  console.log(posts)
   return (
     <div className="flex flex-col h-screen">
       <Header />
-      <main className="flex-grow flex items-center justify-center">
-        <h1 className="text-3xl font-bold">Posts Page</h1>
+      <main className="p-4">
+        <div className="flex justify-between">
+          <h1 className="text-2xl font-semibold">Post Content</h1>
+          <button className="bg-blue-700 px-4 py-2 rounded-full text-white">Add Post</button>
+        </div>
+        <div className="grid grid-cols-3 gap-2">
+          {posts?.map(({ title, body, userId, id }) => (
+            <PostCard
+              key={id}
+              title={title}
+              body={body}
+              userId={userId}
+              id={id}
+            />
+          ))}
+        </div>
       </main>
     </div>
   );
 };
 
-export default PostsPage;
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts: PostProps[] = await response.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
+export default Posts;
